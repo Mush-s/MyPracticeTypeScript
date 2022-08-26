20e5d587f3eb9d8692a250fe657e172fc3b2545e
Mush__s <youtan1223@icloud.com>
2022-08-26 14:56:34 +1000

    comittion

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom";
-import GuestList from "./state/GuestList";
-import UserSearch from "./state/UserSearch";
+import EventComponent from "./event/EventComponent";
+import UserSearch from "./refs/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <GuestList />
       <UserSearch />
+      <EventComponent />
     </div>
   );
 };
